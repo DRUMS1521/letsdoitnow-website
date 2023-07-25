89eaa870ee1b4fff071ba351be70c848947c52f9
Daniela Gómez Noriega <93901985+califfornia@users.noreply.github.com>
2023-07-25 12:26:30 -0500

    portafolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,7 +2,38 @@
   <div class="wrapper-port">
     <h1 class="subtitle">Por<span class="rectangle">tafolio</span></h1>
     <p>Aquí encontrarás una selección de los proyectos en los que hemos tenido el honor de trabajar.<br>¿Estás listo para ser nuestro próximo caso de éxito? ¡Hagámoslo ahora!</p>
-    <img style="width: 500px;" src="../assets/mantenimiento.svg" alt="">
+    
+    <div class="card-portfolio">
+      <h3>01/CARRAZO.PE</h3>
+      <p style="margin-block-end: 1em;">Reinventando la compra y venta de vehículos con tecnología personalizada</p>
+      <div class="imgs-port">
+       <img src="../assets/carrazo-port.png" alt="" style="width: 600px;"> 
+       <img src="../assets/carrazo-port-2.png" alt="" style="width: 600px;"> 
+      </div>
+      <button class="btn-underline" style="font-size: medium;">Accede a más información aquí</button>
+    </div>
+    <div class="line-port"></div>
+
+    <div class="card-portfolio">
+      <h3>02/PLANIUM.AI</h3>
+      <p style="margin-block-end: 1em;">La solución ERP para constructores</p>
+      <div class="imgs-port">
+       <img src="../assets/planium-port.png" alt="" style="width: 600px;"> 
+      </div>
+      <button class="btn-underline" style="font-size: medium;">Accede a más información aquí</button>
+    </div>
+    <div class="line-port"></div>
+
+    <div class="card-portfolio">
+      <h3>03/AJPENNY</h3>
+      <p style="margin-block-end: 1em;">Revolucionando la Producción de Persianas con un ERP a Medida</p>
+      <div class="imgs-port">
+       <img src="../assets/ajpenny-port.png" alt="" style="width: 600px;"> 
+      </div>
+      <button class="btn-underline" style="font-size: medium;">Accede a más información aquí</button>
+    </div>
+    <div class="line-port"></div>
+
   </div>
   </template>
   
@@ -17,4 +48,27 @@
     margin: 2rem;
     text-align: start;
   }
+
+  .card-portfolio{
+    margin: 2rem 0;
+  }
+
+  .card-portfolio h3{
+    color: #333333;
+  }
+
+  .line-port{
+  margin: 5rem 0;
+  width: 100%;
+  height: 1px;
+  background-color: #4d4d4d;
+}
+
+.imgs-port{
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+  margin: 2rem 0;
+}
+
 </style>
