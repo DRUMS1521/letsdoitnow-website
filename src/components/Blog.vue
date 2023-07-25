337a8a7a2e738ea6ccec46d7d8a964891cfb4c49
Daniela Gómez Noriega <93901985+califfornia@users.noreply.github.com>
2023-07-25 17:28:55 -0500

    blog resume

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,18 +1,59 @@
 <template>
-  <div>
-    <img class="mantenimiento" src="../assets/mantenimiento.svg" alt="">
-  </div>
-  </template>
+    <div style="margin-top: 2rem;">
+    <CardBlogCategory :text="'Tech'" :text2="'Inteligencia Artificial'" :text3="'Educación'" :text4="'Webinars'" :text5="'Eventos'"/>
+    </div>
+    <div class="wrapper-blog">
+      <div class="last-entry">
+        <h1 class="subtitle">Norem ipsum dolor sit amet, consectetur adipiscing elit. <span class="rectangle">Etiam eu turpis</span> molestie, dictum est a</h1>
+        <p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. </p>
+        <button class="btn-underline" style="margin-top: 2rem;">Lee la entrada aquí</button>
+      </div>
+      <div>
+        <img src="../assets/img-blog-background.png" alt="">
+      </div>
+    </div>
+
+    <div class="line-port"></div>
+
+    <div class="wrapper-blog-2">
+      <CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'"/>
+      <CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'"/>
+      <CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'"/>
+    </div>
+
+</template>
   
   <script>
+  import CardBlogCategory from './cards/cardBlogCategory.vue';
+  import CardBlogResume from './cards/cardBlogResume.vue';
   export default {
+
+    components: {
+            
+            CardBlogResume,
+            CardBlogCategory
+        }
     
   };
   </script>
 
   <style>
-  .mantenimiento{
-    width: 600px;
-    height: 600px;
+  .wrapper-blog{
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    padding: 0 4rem;
   }
+
+  .last-entry{
+    margin: 3rem;
+  }
+
+  .wrapper-blog-2{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+  }
+
 </style>
