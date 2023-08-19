1d706927447b9cb9ef5550a54220df4f9e3f3e5e
Edimega <86736545+Edimega@users.noreply.github.com>
2023-08-19 00:44:20 -0500

    Prueba notion

--- src/components/Blog/Blog.vue
+++ src/components/Blog/Blog.vue
@@ -1,46 +1,64 @@
 <script setup lang="ts">
-import CardBlogCategory from '../cards/cardBlogCategory.vue';
-import CardBlogResume from '../cards/cardBlogResume.vue';
+	import { ref, onMounted } from 'vue';
+	import { dataNotion } from '../../stores/Notion.ts';
+	import CardBlogCategory from '../cards/cardBlogCategory.vue';
+	import CardBlogResume from '../cards/cardBlogResume.vue';
+
+
+	const documentos = ref([]);
+
+	const getAllArticles = async () => {
+		try {
+			documentos.value = await dataNotion();
+			console.log(documentos.value);
+		} catch (error) {
+			console.error("Error al cargar los documentos:", error);
+		}
+	};
+
+	onMounted(async () => {
+		await getAllArticles();
+	});
 </script>
 
 <template>
-    <div class="wrapper-blog">
-      <div class="last-entry">
-        <h1 class="subtitle">Norem ipsum dolor sit amet, consectetur adipiscing elit. <span class="rectangle">Etiam eu turpis</span> molestie, dictum est a</h1>
-        <p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. </p>
-        <button class="btn-underline" style="margin-top: 2rem;">Lee la entrada aquí</button>
-      </div>
-      <div>
-        <img src="../../assets/img-blog-background.png" alt="">
-      </div>
-    </div>
+	<div class="wrapper-blog">
+		<div class="last-entry">
+			<h1 class="subtitle">Norem ipsum dolor sit amet, consectetur adipiscing elit. <span class="rectangle">Etiam eu turpis</span> molestie, dictum est a</h1>
+			<p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum  est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus.</p>
+			<button class="btn-underline" style="margin-top: 2rem;">Lee la entrada aquí</button>
+		</div>
+		<div>
+			<img src="../../assets/img-blog-background.png" alt="">
+		</div>
+	</div>
 
-    <div class="line-port"></div>
+	<div class="line-port"></div>
 
-    <div class="wrapper-blog-2">
-      <CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'"/>
-      <CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'"/>
-      <CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'"/>
-    </div>
+	<div class="wrapper-blog-2">
+		<CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'" />
+		<CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'" />
+		<CardBlogResume :title="'El cambio inevitable'" :category="'Inteligencia Artificial'" :nameCreator="'Marysabel'" :timeRead="'8min'" />
+	</div>
 
 </template>
 
 <style>
-.wrapper-blog{
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    align-items: center;
-    padding: 0 4rem;
-  }
+	.wrapper-blog {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		align-items: center;
+		padding: 0 4rem;
+	}
 
-  .last-entry{
-    margin: 3rem;
-  }
+	.last-entry {
+		margin: 3rem;
+	}
 
-  .wrapper-blog-2{
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    align-items: center;
-    justify-items: center;
-  }
+	.wrapper-blog-2 {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+		align-items: center;
+		justify-items: center;
+	}
 </style>
