2c016bfe991cefa79da19a7fa73e482be4cbed0a
Edimega <86736545+Edimega@users.noreply.github.com>
2023-08-22 12:40:09 -0500

    build

--- dist/assets/Toast-437c90e0.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-a1a3bc01.js";const r=(t,o,s,a)=>{e()(t,{type:o,timeout:s,position:a})};export{r as s};
--- dist/assets/send-b4cecd4b.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/send-57a0127e.svg";export{s as _};
