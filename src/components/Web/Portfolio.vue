4bd500b0a55b22f54be4bf42b7a6f691a62be00a
Edimega <86736545+Edimega@users.noreply.github.com>
2023-09-11 13:01:12 -0500

    Corrección vista previa portafolio

--- src/components/Web/Portfolio.vue
+++ src/components/Web/Portfolio.vue
@@ -50,7 +50,8 @@
 		<div v-for="(article, i) in documentos.results" :key="i">
 			<div class="grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center items-center px-16 text-left mt-2">
 				<div class="text-center" v-if="(i + 1) % 2 == 0" @click="router.push(`/portfolio/${article.id}`)">
-					<img :src="article.cover?.external?.url" class="cursor-pointer" alt="" style="max-width: 100%; max-height: 350px;">
+					<img v-if="article?.cover?.external" :src="article.cover?.external?.url" class="cursor-pointer" alt="" style="max-width: 100%; max-height: 350px;">
+					<img v-else :src="article.cover?.file?.url" class="cursor-pointer" alt="" style="max-width: 100%; max-height: 350px;">
 				</div>
 				<div class="my-12 cursor-pointer" @click="router.push(`/portfolio/${article.id}`)">
 					<h2 class="subtitle">{{ article.properties?.Titulo?.title[0]?.plain_text }}</h2>
@@ -58,7 +59,8 @@
 					<a class="btn-underline" :href="'/portfolio/' + article.id">Lee la entrada aquí</a>
 				</div>
 				<div class="text-center" v-if="(i + 1) % 2 != 0" @click="router.push(`/portfolio/${article.id}`)">
-					<img :src="article.cover?.external?.url" class="cursor-pointer" alt="" style="max-width: 100%; max-height: 350px;">
+					<img v-if="article?.cover?.external" :src="article.cover?.external?.url" class="cursor-pointer" alt="" style="max-width: 100%; max-height: 350px;">
+					<img v-else :src="article.cover?.file?.url" class="cursor-pointer" alt="" style="max-width: 100%; max-height: 350px;">
 				</div>
 			</div>
 			<div style="box-shadow: 1px 5px 5px; height: 1px; width: 90%; margin: 2rem auto;" v-if="documentos.results.length - 1 != i"></div>
