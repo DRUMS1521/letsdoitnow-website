667e11dae7d8478aee3a4738c48a5dc5a466e4cd
Edimega <86736545+Edimega@users.noreply.github.com>
2023-10-19 03:17:33 -0500

    Form

--- src/components/Web/Contact.vue
+++ src/components/Web/Contact.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import axios from 'axios';
+import { showToast, POSITION } from '../../stores/Toast';
 /* import { ref, onMounted } from 'vue';
 import { lang } from '@/stores/SwitchLang'
 import { seoMeta } from '@/stores/SeoMeta';
@@ -11,6 +14,61 @@ onMounted(() => {
 	language.value = lang(localStorage.getItem("Lang") === "Es").porfolio
 	seoMeta(language.value.langweb, language.value.titleweb, language.value.desciptionweb, language.value.imgweb, language.value.keywords);
 }) */
+
+// Definición de variables para del formulario
+const name = ref('');
+const email = ref('');
+const company = ref('');
+const industry = ref('');
+const description = ref('');
+const requirements = ref('');
+const mvp = ref('');
+const team = ref('');
+const investment = ref('');
+const message = ref('');
+
+// Función para enviar el formulario
+const enviarFormulario = async () => {
+    const googleFormsURL = 'http://localhost:3000/api/googleForm';
+
+  // Crea un objeto con los datos del formulario
+    const formData = {
+    'name': name.value,
+    'email': email.value,
+    'company': company.value,
+    'industry': industry.value,
+    'description': description.value,
+
+    'requirements': requirements.value,
+    'mvp': mvp.value,
+    'team': team.value,
+    'investment': investment.value,
+    'message': message.value,
+    };
+
+    try {
+        const response = await axios.post(googleFormsURL, formData);
+        if (response.status === 200) {
+        showToast(`Formulario enviado con éxito`, 'success', 3000, POSITION.BOTTOM_CENTER)
+        // Restablece los campos del formulario
+        name.value = '';
+        email.value = '';
+        company.value = '';
+        industry.value = '';
+        description.value = '';
+        requirements.value = '';
+        mvp.value = '';
+        team.value = '';
+        investment.value = '';
+        message.value = '';
+        } else {
+        showToast(`Error al enviar el formulario. Por favor, inténtalo de nuevo.`, 'error', 3000, POSITION.BOTTOM_CENTER)
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        showToast(`Hubo un problema al enviar el formulario. Por favor, inténtalo de nuevo más tarde.`, 'error', 3000, POSITION.BOTTOM_CENTER)
+    }
+};
 
 const whatsapp = (): void => {
 	window.open("https://api.whatsapp.com/send?phone=15512612985&text=Hola!%20%F0%9F%91%8B%F0%9F%8F%BC%20", '_blank')
@@ -38,38 +96,43 @@ const linkedin = (): void => {
     </div> -->
 
     <div class="w-[600px] mx-auto">
-        <form action="">
-            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="name" id="name" placeholder="Nombre completo">
+        <form @submit.prevent="enviarFormulario" class="mt-8 mb-16">
+            <label for="name">Nombre completo</label>
+            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="name" id="name" placeholder="Name" v-model="name">
 
-            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="email" id="email" placeholder="Correo electrónico">
+            <label for="email">Correo electrónico</label>
+            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="email" id="email" placeholder="Email" v-model="email">
 
-            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="company" id="company" placeholder="Nombre de tu empresa">
+            <label for="company">Nombre de tu empresa</label>
+            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="company" id="company" placeholder="Company" v-model="company">
 
-            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="industry" id="industry" placeholder="Industria de tu empresa">
+            <label for="">Industria de tu empresa</label>
+            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="industry" id="industry" placeholder="Industry" v-model="industry">
 
-            <input class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" type="text" name="phone" id="phone" placeholder="Teléfono">
+            <label for="description">¿Podrías describirnos brevemente tu proyecto? Queremos entender tus necesidades y cómo nuestra tecnología puede ayudarte</label>
+            <textarea class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="description" id="description" cols="1" rows="1" placeholder="Description" v-model="description"></textarea>
 
-            <label for="">¿Podrías describirnos brevemente tu proyecto? Queremos entender tus necesidades y cómo nuestra tecnología puede ayudarte</label>
-            <textarea class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="description" id="description" cols="1" rows="1" placeholder="Mensaje"></textarea>
+            <label for="Requirements">¿Tienes un levantamiento de requerimientos que puedas compartir con nosotros para entender mejor lo que quieres hacer?<br>Escríbenos cuál es o adjunta un link donde podamos ver el documento.</label>
+            <textarea class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="Requirements" id="Requirements" cols="1" rows="1" placeholder="Requirements" v-model="requirements"></textarea>
 
-            <label for="">¿Tienes un levantamiento de requerimientos que puedas compartir con nosotros para entender mejor lo que quieres hacer?<br>Escríbenos cuál es o adjunta un link donde podamos ver el documento.</label>
-            <textarea class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="message" id="message" cols="1" rows="1" placeholder="Mensaje"></textarea>
-
-            <label for="">¿Es este un proyecto completamente nuevo o ya existe un MVP o una app en funcionamiento?</label>
-            <select class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="" id="">
-                <option value="">Idea nueva</option>
-                <option value="">MVP o una app en funcionamiento</option>
-                <option value="">Aplicación en funcionamiento</option>
+            <label for="mvp">¿Es este un proyecto completamente nuevo o ya existe un MVP o una app en funcionamiento?</label>
+            <select class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="mvp" id="mvp" v-model="mvp">
+                <option value="">Seleccione una opción</option>
+                <option value="Idea nueva">Idea nueva</option>
+                <option value="MVP">MVP o una app en funcionamiento</option>
+                <option value="Aplicación en funcionamiento">Aplicación en funcionamiento</option>
             </select>
-
-            <label for="">¿Cuentas con un equipo de ingeniería y buscas que complementemos su trabajo, o buscas que seamos tu equipo de ingeniería?</label>
-            <select class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="" id="">
-                <option value="">Tengo un equipo y necesito apoyo</option>
-                <option value="">Necesito un equipo de ingeniería</option>
+            
+            <label for="team">¿Cuentas con un equipo de ingeniería y buscas que complementemos su trabajo, o buscas que seamos tu equipo de ingeniería?</label>
+            <select class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="team" id="team" v-model="team">
+                <option value="">Seleccione una opción</option>
+                <option value="Tengo un equipo">Tengo un equipo y necesito apoyo</option>
+                <option value="Necesito un equipo">Necesito un equipo de ingeniería</option>
             </select>
-
-            <label for="">Por favor, selecciona el rango de inversión que consideras para tu proyecto. Esto nos ayudará a entender tus expectativas y a proporcionarte una cotización más precisa<br>Estos rangos se encuentras en dólar</label>
-            <select class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="" id="">
+            
+            <label for="investment">Por favor, selecciona el rango de inversión que consideras para tu proyecto. Esto nos ayudará a entender tus expectativas y a proporcionarte una cotización más precisa<br>Estos rangos se encuentras en dólar</label>
+            <select class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="investment" id="investment" v-model="investment">
+                <option value="">Seleccione una opción</option>
                 <option value="$6,000 - $12,000">$6,000 - $12,000</option>
                 <option value="$12,000 - $20,000">$12,000 - $20,000</option>
                 <option value="$20,000 - $25,000">$20,000 - $25,000</option>
@@ -80,10 +143,12 @@ const linkedin = (): void => {
                 <option value="$70,000 - $90,000">$70,000 - $90,000</option>
             </select>
 
-            <label for="">¿Algún comentario adicional?<br>Si tienes alguna otra información que creas que debamos saber, por favor indícala aquí</label>
-            <textarea class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="message" id="message" cols="1" rows="1" placeholder="Mensaje"></textarea>
+            <label for="message">¿Algún comentario adicional?<br>Si tienes alguna otra información que creas que debamos saber, por favor indícala aquí</label>
+            <textarea class="w-full pr-12 border border-[0.5px] border-gray-400 rounded-[10px] p-[0.7rem] [1.2rem] outline-none mb-6" name="message" id="message" cols="1" rows="1" placeholder="Mensaje" v-model="message"></textarea>
 
-            <input type="submit" value="Enviar mensaje" class="bg-[#263F28] text-white p-[0.7rem] [1.2rem] rounded-[10px]">
+            <div class="w-100 text-right">
+                <input type="submit" value="Enviar mensaje" class="bg-[#263F28] text-white p-[0.7rem] [1.2rem] rounded-[10px]">
+            </div>
         </form>
     </div>
 
