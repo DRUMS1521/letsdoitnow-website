25e6c944a141c285bf872398815ef397c545cb93
Jesus Bastidas <davidbastidascarvajal@gmail.com>
2023-11-22 22:56:02 -0500

    dia 4 listo

--- src/components/Web/DesignSprint/Dia4DesignSprint.vue
+++ src/components/Web/DesignSprint/Dia4DesignSprint.vue
@@ -3,4 +3,57 @@
 
 <template>
     <h1>Dia 4</h1>
+    <div>
+        <P class="ml-1 font-semibold my-6">Tareas</P>
+        <div class="md:flex w-full shadow-xl p-3 px-2 rounded-md bg-white mb-6">
+            <div class="w-full md:w-1/4">
+                <p class="font-medium">Miembro del equipo</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+            <div class="w-full my-2 md:w-2/4 md:mx-4 md:my-0">
+                <p class="font-medium">Tarea</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+            <div class="w-full md:w-1/4">
+                <p class="font-medium">Link de entrega</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+        </div>
+        <div class="md:flex w-full shadow-xl p-3 px-2 rounded-md bg-white mb-6">
+            <div class="w-full md:w-1/4">
+                <p class="font-medium">Miembro del equipo</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+            <div class="w-full my-2 md:w-2/4 md:mx-4 md:my-0">
+                <p class="font-medium">Tarea</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+            <div class="w-full md:w-1/4">
+                <p class="font-medium">Link de entrega</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+        </div>
+        <div class="md:flex w-full shadow-xl p-3 px-2 rounded-md bg-white mb-6">
+            <div class="w-full md:w-1/4">
+                <p class="font-medium">Miembro del equipo</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+            <div class="w-full my-2 md:w-2/4 md:mx-4 md:my-0">
+                <p class="font-medium">Tarea</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+            <div class="w-full md:w-1/4">
+                <p class="font-medium">Link de entrega</p>
+                <input class="border border-gray-900 rounded-md px-8 py-3 w-full" type="text" placeholder="Escribe tu respuesta">
+            </div>
+        </div>
+        <div class="w-full mt-4">
+            <button class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded-md flex ml-auto">
+                Siguiente
+                <span class="ml-2">
+                    <img src="@/assets/Arrow-2.svg" alt="Siguiente" class="w-6 h-6" />
+                </span>
+            </button>
+        </div>
+    </div>
 </template>
