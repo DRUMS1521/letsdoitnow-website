546cace8bd364a980832c8c7ca13f3b4872183d9
Jesus Bastidas <davidbastidascarvajal@gmail.com>
2023-11-23 10:28:31 -0500

    vista resumen

--- src/components/Web/DesignSprint/Dia5DesignSprint.vue
+++ src/components/Web/DesignSprint/Dia5DesignSprint.vue
@@ -2,46 +2,46 @@
 </script>
 
 <template>
-    <h1>Dia 5</h1>
     <div>
         <div>
-            <div class="my-2 md:my-6">
-                <p class="ml-1 font-semibold my-6">Feedback: <span class="font-semibold text-green-600">Solución # 1</span></p>
+            <div class="mb-8">
+                <p class="text-lg font-semibold">¡Haz finalizado el sprint!</p>
+                <p class="font-medium">Aqui encontraras un resumen de tu desing sprint</p>
             </div>
-            <div>
-                <p class="mb-2 font-medium">Descripción de la solución</p>
-                <textarea class="border border-gray-800 rounded-md p-1 w-full" name="" id="" cols="30" rows="7"></textarea>
-            </div>
-        </div>
-        <div class="my-4">
-            <div class="mb-2 ml-1 font-semibold">
-                <p>Usuario 1</p>
-            </div>
-            <div class="md:w-full md:flex">
-                <div class="mb-2 md:w-1/5 md:mr-2">
-                    <p class="font-medium">Lo que disfrutó</p>
-                    <input class="border border-gray-800 rounded-md w-full py-1" type="text">
-                </div>
-                <div class="md:w-2/5 md:mr-2">
-                    <p class="font-medium">Posibles mejoras</p>
-                    <input class="border border-gray-800 rounded-md w-full py-1" type="text">
-                </div>
-                <div class="md:w-2/5">
-                    <p class="font-medium">Lo que disfrutó</p>
-                    <input class="border border-gray-800 rounded-md w-full py-1" type="text">
+            <div class="flex justify-between items-center px-4 p-1 py-2 bg-green-300 rounded-md w-full mb-4">
+                <p class="text-xl font-semibold">Día 1</p>
+                <div>
+                    <img src="@/assets/expand_more.svg" alt="">
                 </div>
             </div>
-        </div>
-        <div class="w-full mt-4 flex justify-center md:flex md:items-center">
-            <button class="bg-green-500 hover:scale-110 p-2 rounded-full">
-                <span>
-                    <img src="@/assets/suma1.png" alt="Siguiente" class="w-6 h-6" />
-                </span>
-            </button>
+            <div class="flex justify-between items-center px-4 p-1 py-2 bg-green-300 rounded-md w-full mb-4">
+                <p class="text-xl font-semibold">Día 2</p>
+                <div>
+                    <img src="@/assets/expand_more.svg" alt="">
+                </div>
+            </div>
+            <div class="flex justify-between items-center px-4 p-1 py-2 bg-green-300 rounded-md w-full mb-4">
+                <p class="text-xl font-semibold">Día 3</p>
+                <div>
+                    <img src="@/assets/expand_more.svg" alt="">
+                </div>
+            </div>
+            <div class="flex justify-between items-center px-4 p-1 py-2 bg-green-300 rounded-md w-full mb-4">
+                <p class="text-xl font-semibold">Día 4</p>
+                <div>
+                    <img src="@/assets/expand_more.svg" alt="">
+                </div>
+            </div>
+            <div class="flex justify-between items-center px-4 p-1 py-2 bg-green-300 rounded-md w-full mb-4">
+                <p class="text-xl font-semibold">Día 5</p>
+                <div>
+                    <img src="@/assets/expand_more.svg" alt="">
+                </div>
+            </div>
         </div>
         <div class="w-full mt-4">
             <button class="bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded-md flex ml-auto">
-                Finalizar Sprint
+                Volver al Inicio
             </button>
         </div>
     </div>
